0b97dafb19076c4b1be2839f12cb5304b649e447
Dhea Armalivia <daffyderr@gmail.com>
2024-02-09 15:12:25 +0700

    Initial Commit

---
+++ src/main/java/com/bank/transfersystem/banktransfersystem/BanktransfersystemApplication.java
@@ -0,0 +1,13 @@
+package com.bank.transfersystem.banktransfersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanktransfersystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanktransfersystemApplication.class, args);
+	}
+
+}
